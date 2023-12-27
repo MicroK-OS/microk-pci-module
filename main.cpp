5094f4966848e29a49bf9e9ca0a225c45a2b06b0
Filippo Mutta <filippo.mutta@gmail.com>
2023-12-27 16:42:30 +0100

    Last Changes.

--- main.cpp
+++ main.cpp
@@ -15,41 +15,8 @@
 extern "C" uint32_t VendorID = 0xCAFEBABE;
 extern "C" uint32_t ProductID = 0xB830C0DE;
 
-
-int MessageHandler(MKMI_Message *msg, uint64_t *data) {
-	if (*data) {
-		MKMI_Printf("MCFG at 0x%x\r\n", *data);
-
-		Syscall(SYSCALL_MODULE_SECTION_REGISTER, "PCI", VendorID, ProductID, 0, 0 ,0);
-		Syscall(SYSCALL_MODULE_SECTION_REGISTER, "PCIe", VendorID, ProductID, 0, 0 ,0);
-
-		EnumeratePCI(*data);
-
-		return 0;
-	} else {
-		MKMI_Printf("No MCFG found.\r\n");
-
-		return 1;
-	}
-
-	return 1;
-}
-
 extern "C" size_t OnInit() {
-	SetMessageHandlerCallback(MessageHandler);
-	Syscall(SYSCALL_MODULE_MESSAGE_HANDLER, MKMI_MessageHandler, 0, 0, 0, 0, 0);
-
-	uint32_t pid = 0, vid = 0;
-	Syscall(SYSCALL_MODULE_SECTION_GET, "ACPI", &vid, &pid, 0, 0 ,0);
-	MKMI_Printf("ACPI -> VID: %x PID: %x\r\n", vid, pid);
-
-	uint8_t msg[256];
-	uint64_t *data = (uint64_t*)((uintptr_t)msg + 128);
-	*data = 0x69696969;
-
-	SendDirectMessage(vid, pid, msg, 256);
-
-	Free(msg);
+	EnumeratePCI(-1);
 
 	return 0;
 }
